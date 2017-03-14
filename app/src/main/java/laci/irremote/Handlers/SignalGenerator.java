6f94ca794daaddf9f78114e545ed928bf360d352
Chavife <ladislav.feldsam@gmail.com>
2017-03-14 15:50:02 +0100

    SignalGenerator Handler added and developed. Now it can generate and emit IR signal

---
+++ app/src/main/java/laci/irremote/Handlers/SignalGenerator.java
@@ -0,0 +1,122 @@
+package laci.irremote.Handlers;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+
+/**
+ * This file contains Handlers for generating specific
+ * frequencies, horizontal alignments, frequency length etc.
+ */
+
+public class SignalGenerator {
+
+
+
+    private double duration = 0; // seconds
+    private final int sampleRate = 192000;
+    private int numSamples = (int) (duration * sampleRate);
+    private double sample[];
+    private double Frequency = 18000; // Hz
+    private double HorizontalOffset = 90.0; // Degree
+    private double PulseOffsetPerc = 0.0; // Percent/100
+
+    private byte MonoGeneratedSnd[];
+    private byte StereoGeneratedSnd[];
+
+    /**
+     * @param Frequency
+     *
+     * Constructor for Frequency Generator
+     * Frequency parameter gets an Int value of the desired Frequency to generate in Hz
+     */
+    public SignalGenerator(int Frequency) {
+        this.Frequency = Frequency;
+    }
+
+    public void Emit(){
+        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_CONFIGURATION_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT, numSamples*4,
+                AudioTrack.MODE_STATIC);
+        audioTrack.setStereoVolume(1f, 1f);
+        audioTrack.write(StereoGeneratedSnd, 0, StereoGeneratedSnd.length);
+        audioTrack.play();
+    }
+
+    public void SetHorOff(double offset){
+        HorizontalOffset = offset;
+    }
+
+    public void SetPulseOff(double OffsetinPercent){
+        PulseOffsetPerc = OffsetinPercent/100.0;
+    }
+
+
+
+    public void Generate(double Signal[]){
+
+
+        int SignalinSampleLength[] = new int[Signal.length];
+
+        numSamples = 0;
+
+        for(int i = 0; i < Signal.length; i++){
+            SignalinSampleLength[i] = (int) (Signal[i]/10000 * sampleRate);
+            numSamples += SignalinSampleLength[i];
+        }
+
+        duration = (numSamples * 1.0)/sampleRate;
+        sample = new double[numSamples];
+        MonoGeneratedSnd = new byte[2 * numSamples];
+        StereoGeneratedSnd = new byte[4 * numSamples];
+
+
+        double Period = (sampleRate/ Frequency);
+        double HalfPeriodCounter = Period/2.0;
+        double PulseOffset = (Period/2.0) * (-PulseOffsetPerc);
+        boolean LogicalOne = true;
+        int position = 0;
+        for(int i = 0; i < Signal.length; i++){
+
+            for(int j = 0; j< SignalinSampleLength[i]; j++){
+
+                if(i%2 == 0){ //generate carrier frequency
+                    if(position < HalfPeriodCounter + PulseOffset && LogicalOne) sample[position] = 1;
+                    else if (position < HalfPeriodCounter - PulseOffset && !LogicalOne) sample[position] = -1;
+                    else{
+                        sample[position] = sample[position-1];
+                        LogicalOne = !LogicalOne;
+                        HalfPeriodCounter += Period/2.0;
+                    }
+                }else{ //don`t generate
+                    sample[position] = 0;
+                }
+                position++;
+            }
+            if(i%2 == 1) HalfPeriodCounter += SignalinSampleLength[i];
+        }
+
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (double dVal : sample) {
+            short val = (short) (dVal * 32767);
+            MonoGeneratedSnd[idx++] = (byte) (val & 0x00ff);
+            MonoGeneratedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+        }
+
+        int HorizontalOffset = (int)((this.HorizontalOffset /360.0) * (sampleRate/ Frequency) * 2.0);
+
+        int j = 0;
+        for(int i = HorizontalOffset; i < MonoGeneratedSnd.length-2; i+=2){
+            StereoGeneratedSnd[j++] = MonoGeneratedSnd[i];
+            StereoGeneratedSnd[j++] = MonoGeneratedSnd[i+1];
+            StereoGeneratedSnd[j++] = MonoGeneratedSnd[(i - HorizontalOffset)];
+            StereoGeneratedSnd[j++] = MonoGeneratedSnd[(i+1) - HorizontalOffset];
+        }
+
+    }
+
+}
--- app/src/main/java/laci/irremote/TestActivity.java
+++ app/src/main/java/laci/irremote/TestActivity.java
@@ -1,21 +1,37 @@
 package laci.irremote;
 
-import android.media.AudioManager;
-import android.media.ToneGenerator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import laci.irremote.Handlers.SignalGenerator;
 
 public class TestActivity extends AppCompatActivity {
+
+    SignalGenerator FG;
+    EditText horoffset;
+    EditText pulseoff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        FG = new SignalGenerator(18000);
+        horoffset = (EditText) findViewById(R.id.hor_offset);
+        pulseoff = (EditText) findViewById(R.id.pulse_offset);
+        horoffset.setText(90 + "");
+        pulseoff.setText(25+"");
     }
 
-    public void GenerateFrequency(View v){
-        ToneGenerator TG = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
-        TG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD);
+    public void GenerateFrequency(View v) throws InterruptedException {
+        //ToneGenerator TG = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+        //TG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD);
+        double Signal[] = {24,6,6,6.5,11,7,5.5,6,6,6,13,6,6,6,6,6,6,6.5,6,6,6,6,6,6,12,6,12,6,6,6,6,6};
+        //double Signal[] = {30000};
+        FG.SetHorOff(Integer.parseInt(horoffset.getText().toString()));
+        FG.SetPulseOff(Integer.parseInt(pulseoff.getText().toString()));
+        FG.Generate(Signal);
+        FG.Emit();
     }
 }
